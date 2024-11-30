8bf99c3b99a7cc2bbbb4ab1a9b9b644c072d96ff
L4B0MB4 <metalfan65@gmail.com>
2024-11-30 16:36:59 +0100

    Refactor event polling logic to improve readability and modularity by extracting PollEvent method

--- pkg/eventpolling/polling.go
+++ pkg/eventpolling/polling.go
@@ -23,44 +23,41 @@ func NewEventPolling(client *client.EventSourcingHttpClient, eventRepo *reposito
 
 func (ep *EventPolling) PollEvents() {
 
-	hadMoreThenZeroEvents := true
 	for {
-		if hadMoreThenZeroEvents {
-			time.Sleep(100 * time.Millisecond)
-		} else {
-			time.Sleep(500 * time.Millisecond)
-		}
-		eId, err := ep.eventRepo.GetLastEvent()
-		if err != nil {
-			log.Err(err).Msg("Error while getting last events")
-			continue
-		}
-		events, err := ep.client.GetEventsSince(eId, 2)
-		if err != nil {
-			log.Err(err).Msg("Error while polling events")
-			continue
-		}
+		time.Sleep(100 * time.Millisecond)
+		ep.PollEvent()
+	}
 
-		for _, event := range events {
+}
 
-			err := ep.eventHandler.HandleEvent(event)
-			if err != nil {
-				log.Err(err).Msg("Error while processing event")
-				break
-			}
-		}
-		if len(events) == 0 {
-			hadMoreThenZeroEvents = true
-			continue
-		}
-		hadMoreThenZeroEvents = false
-		//will this break the db consistency if there are going to be multiple instances of this service?
-		// probably but if we dont a volume (that both instances use as a db file) this should be fine
-		err = ep.eventRepo.ReplaceEvent(events[len(events)-1].Id)
+func (ep *EventPolling) PollEvent() {
+	eId, err := ep.eventRepo.GetLastEvent()
+	if err != nil {
+		log.Err(err).Msg("Error while getting last events")
+		return
+	}
+	events, err := ep.client.GetEventsSince(eId, 2)
+	if err != nil {
+		log.Err(err).Msg("Error while polling events")
+		return
+	}
+
+	for _, event := range events {
+
+		err := ep.eventHandler.HandleEvent(event)
 		if err != nil {
-			log.Err(err).Msg("Error while replacing event")
+			log.Err(err).Msg("Error while processing event")
 			break
 		}
 	}
+	if len(events) == 0 {
+		return
+	}
+	//will this break the db consistency if there are going to be multiple instances of this service?
+	// probably but if we dont a volume (that both instances use as a db file) this should be fine
+	err = ep.eventRepo.ReplaceEvent(events[len(events)-1].Id)
+	if err != nil {
+		log.Err(err).Msg("Error while replacing event")
 
+	}
 }
